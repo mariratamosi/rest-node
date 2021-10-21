2e30363181bab93c554791e60fcd62102915f838
marira <rongrazzo@gmail.com>
2021-10-21 22:59:02 +0600

    schema setup

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from "express";
 import routes from "./src/routes/crmRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://rima:1234@cluster0.fku13.mongodb.net/rest-node?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+//bodyparser setup
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
