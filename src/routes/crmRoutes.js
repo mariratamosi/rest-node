bd75c469fafa75f90a7687c1c03e9800c1a0382d
marira <rongrazzo@gmail.com>
2021-10-21 22:48:45 +0600

    middleware introduced

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,9 +1,17 @@
 const routes = (app) => {
   app
     .route("/contact")
-    .get((req, res) => {
-      res.send("Get the contacts");
-    })
+    .get(
+      (req, res, next) => {
+        //middleware //might be thirdparty
+        console.log(`Request from ${req.originalUrl}`);
+        console.log(`Request type ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("Get the contacts");
+      }
+    )
     .post((req, res) => {
       res.send("Post the contacts");
     });
